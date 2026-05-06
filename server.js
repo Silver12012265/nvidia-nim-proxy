c935a5d120ea3e74f91f160d99a2e2e61475eadd
Tonskie211 <hnca.ramos.up@phinmaed.com>
2026-05-06 10:02:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -233,7 +233,7 @@ app.post('/v1/chat/completions', async (req, res) => {
       model: nimModel,
       messages: messages,
       temperature: temperature || 0.7,
-      max_tokens: max_tokens || 4096,
+      max_tokens: max_tokens || 12000,
       stream: stream || false
     };
 
